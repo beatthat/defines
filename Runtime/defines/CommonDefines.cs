1b16beb6ecd4288907b5032d1c4d6efd7969bd8c
larrykirschner <larrykirschner@gmail.com>
2018-07-08 11:19:24 -0700

    updated template

--- Runtime/defines/CommonDefines.cs
+++ Runtime/defines/CommonDefines.cs
@@ -2,12 +2,12 @@ using System;
 namespace BeatThat.Defines
 {
     //[EditDefine("DEBUG_UNSTRIP", "When defined, log messages normally stripped from builds are unstripped")]
-    [EditDefine(new string[] {
-        "ENV_PRODUCTION",
-        "ENV_DEV",
-        "ENV_DEVLOCAL"
-    }, "configure server env/stage")]
-    public static class CommonDefines
-    {
-    }
+    //[EditDefine(new string[] {
+    //    "ENV_PRODUCTION",
+    //    "ENV_DEV",
+    //    "ENV_DEVLOCAL"
+    //}, "configure server env/stage")]
+    //public static class CommonDefines
+    //{
+    //}
 }
